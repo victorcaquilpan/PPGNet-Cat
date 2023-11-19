ba1944f288874ea926d856ce2fcee5542d2faf08
victorcaquilpan <victor.caquilpan@gmail.com>
2023-11-19 17:35:29 +1030

    using argumentparser

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import torch
 import numpy as np
 from pytorch_lightning import Trainer
 import json
+from argparse import ArgumentParser
 
 # Import other complementary scripts
 from models.pl_inference import ReidPrediction
@@ -10,89 +11,111 @@ from datasets.dataloader import ReidDataModule
 from utils.cosine_similarity import cosine_similarity
 from utils.re_ranking import re_ranking
 from utils.evaluation import evaluate
-from utils.evaluation_tiger import evaluate_tiger
 
-#Parameteres
-class Config():
-    CAT_TESTING_DIR = 'data/test/images/'
-    CAT_ANNO_TEST_FILE = 'data/test/test_anno.csv'
-    EVALUATION_FILE = 'data/test/gt_test_plain.json'
-    NUMBER_WORKERS = 8
-    NUM_CLASSES = 300
-    BATCH_SIZE_TEST = 2
-    TRANSFORMATION = True
-    SIZE_FULL_IMAGE = (256,512)
-    EMBEDDING_SIZE = 2560
-    ARCFACE = False
-    BACKBONE = 'resnet152'
-    DETERMINISTIC = [True, "warn"]
-    PRECISION = '16-mixed'
-    TRAINED_MODEL = 'best_model.pth'
+def main(args):
+    #Parameteres
+    class Config():
+        CAT_TESTING_DIR = args.cat_testing_dir
+        CAT_ANNO_TEST_FILE = args.cat_anno_test_file
+        EVALUATION_FILE = args.evaluation_file
+        NUMBER_WORKERS = args.number_workers
+        NUM_CLASSES = args.num_classes
+        BATCH_SIZE_TEST = args.batch_size_test
+        SIZE_FULL_IMAGE = args.size_full_image
+        EMBEDDING_SIZE = args.embedding_size
+        ARCFACE = args.arcface
+        BACKBONE = args.backbone
+        DETERMINISTIC = args.deterministic
+        PRECISION = args.precision
+        TRAINED_MODEL = args.trained_model
 
-# Creating dataloader
-cat_data = ReidDataModule(data_directory=Config(),
-                            batch_size_test = Config().BATCH_SIZE_TEST,
-                            transform=Config().TRANSFORMATION,
-                            num_workers= Config().NUMBER_WORKERS, 
-                            size_full_image = Config().SIZE_FULL_IMAGE)
-# Call the setup method
-cat_data.setup()
+    # Creating dataloader
+    cat_data = ReidDataModule(data_directory=Config(),
+                                batch_size_test = Config().BATCH_SIZE_TEST,
+                                transform= False,
+                                num_workers= Config().NUMBER_WORKERS, 
+                                size_full_image = Config().SIZE_FULL_IMAGE)
+    # Call the setup method
+    cat_data.setup()
 
-# Create the model
-eval_model = ReidPrediction(
-    backbone_model = Config().BACKBONE,
-    number_classes = Config().NUM_CLASSES, 
-    embedding_size = Config().EMBEDDING_SIZE,
-    arcface = Config().ARCFACE)
+    # Create the model
+    eval_model = ReidPrediction(
+        backbone_model = Config().BACKBONE,
+        number_classes = Config().NUM_CLASSES, 
+        embedding_size = Config().EMBEDDING_SIZE,
+        arcface = Config().ARCFACE)
 
-# Create the trainer
-trainer = Trainer(accelerator='gpu', logger = False, 
-                enable_checkpointing=False, 
-                precision = Config().PRECISION, 
-                deterministic = Config().DETERMINISTIC)
+    # Create the trainer
+    trainer = Trainer(accelerator='gpu', logger = False, 
+                    enable_checkpointing=False, 
+                    precision = Config().PRECISION, 
+                    deterministic = Config().DETERMINISTIC)
 
-# Load the weights and biases
-eval_model.full_image_model.load_state_dict(torch.load('pretrained_weights/' + Config().TRAINED_MODEL))
+    # Load the weights and biases
+    eval_model.full_image_model.load_state_dict(torch.load('pretrained_weights/' + Config().TRAINED_MODEL))
 
-# # Evaluation of model
-predictions = trainer.predict(eval_model,dataloaders=cat_data.test_dataloader())
+    # # Evaluation of model
+    predictions = trainer.predict(eval_model,dataloaders=cat_data.test_dataloader())
 
-# Re-ranking
- 
-# Create a empty matrix
-dist_matrix = np.zeros((len(eval_model.pred_img_id),len(eval_model.pred_img_id)))
+    # Re-ranking
+    
+    # Create a empty matrix
+    dist_matrix = np.zeros((len(eval_model.pred_img_id),len(eval_model.pred_img_id)))
 
-for query_im in range(0, len(eval_model.pred_img_id)):
-    for collection_im in range(0,len(eval_model.pred_img_id)):
-        dist_matrix[query_im,collection_im] = cosine_similarity(eval_model.pred_embeddings[query_im].cpu().numpy(), eval_model.pred_embeddings[collection_im].cpu().numpy())
+    for query_im in range(0, len(eval_model.pred_img_id)):
+        for collection_im in range(0,len(eval_model.pred_img_id)):
+            dist_matrix[query_im,collection_im] = cosine_similarity(eval_model.pred_embeddings[query_im].cpu().numpy(), eval_model.pred_embeddings[collection_im].cpu().numpy())
 
-# Get the reranked distance matrix
-reranked_dist = re_ranking(dist_matrix, dist_matrix, dist_matrix, k1=20, k2=6, lambda_value=0.3)
+    # Get the reranked distance matrix
+    reranked_dist = re_ranking(dist_matrix, dist_matrix, dist_matrix, k1=20, k2=6, lambda_value=0.3)
 
-# Create output list
-prediction_cat_results = []
+    # Create output list
+    prediction_cat_results = []
 
-# Calculate the distance between the query image and each one the the remaining images
-for query_im in range(0, len(eval_model.pred_img_id)):
-    # Create a dictionary
-    dict_query = {"query_id": int(eval_model.pred_img_id[query_im])}
-    # Save the results in a list
-    result_query = []
-    # Calculate the distance over the remaining images
-    for ans_img in range(0, len(eval_model.pred_img_id)):
-        if query_im != ans_img:
-            img_dist = (int(eval_model.pred_img_id[ans_img]) , reranked_dist[query_im,ans_img])
-            result_query.append(img_dist)
-    # Sort images
-    result_query = sorted(result_query, key=lambda x: x[1],reverse=False)
-    result_query = [img[0] for img in result_query]
-    dict_query["ans_ids"] = result_query
-    prediction_cat_results.append(dict_query)
+    # Calculate the distance between the query image and each one the the remaining images
+    for query_im in range(0, len(eval_model.pred_img_id)):
+        # Create a dictionary
+        dict_query = {"query_id": int(eval_model.pred_img_id[query_im])}
+        # Save the results in a list
+        result_query = []
+        # Calculate the distance over the remaining images
+        for ans_img in range(0, len(eval_model.pred_img_id)):
+            if query_im != ans_img:
+                img_dist = (int(eval_model.pred_img_id[ans_img]) , reranked_dist[query_im,ans_img])
+                result_query.append(img_dist)
+        # Sort images
+        result_query = sorted(result_query, key=lambda x: x[1],reverse=False)
+        result_query = [img[0] for img in result_query]
+        dict_query["ans_ids"] = result_query
+        prediction_cat_results.append(dict_query)
 
-# Save the list of dictionaries to the JSON file
-with open('cat_results', "w") as json_file:
-    json.dump(prediction_cat_results, json_file, indent=4)
+    # Save the list of dictionaries to the JSON file
+    with open('cat_results', "w") as json_file:
+        json.dump(prediction_cat_results, json_file, indent=4)
 
-# Evaluation
-print(evaluate(Config().EVALUATION_FILE,'cat_results',phase_codename='dev'))
+    # Evaluation
+    print(evaluate(Config().EVALUATION_FILE,'cat_results',phase_codename='dev'))
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--cat_testing_dir", type=str, default='data/test/images/',
+                        help="Path of cat images for testing")
+    parser.add_argument("--cat_anno_test_file", type=str, default='data/test/test_anno.csv',
+                        help="Filepath of annotations for testing images")
+    parser.add_argument("--evaluation_file", type=str, default='data/test/gt_test_plain.json',
+                        help="Filepath of ground truth")
+    parser.add_argument("--number_workers", type=int, default = 8)
+    parser.add_argument("--num_classes", type = int, default = 300)
+    parser.add_argument("--batch_size_test",type = int, default = 2)
+    parser.add_argument("--size_full_image", type = tuple, default = (256,512))
+    parser.add_argument("--embedding_size", type = int, default = 2560)
+    parser.add_argument("--arcface", type = bool, default = False)
+    parser.add_argument("--backbone", type = str, default= "resnet152")
+    parser.add_argument("--deterministic", type = list, default = [True, "warn"])
+    parser.add_argument("--precision", type = str, default = "16-mixed")
+    parser.add_argument("--trained_model", default = "best_model.pth", help = "Use of weights")
+    args = parser.parse_args()
+
+main(args)       
 
--- train.py
+++ train.py
@@ -2,6 +2,7 @@
 import torch
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
+from argparse import ArgumentParser
 
 # Complementary scripts
 from losses import make_loss_with_parameters
@@ -11,95 +12,131 @@ from models.pl_model import ReidCatModel
 # Set seed
 torch.manual_seed(123) 
 
-#Parameteres
-class Config():
-    CAT_TRAINING_DIR = 'data/train/images/' 
-    CAT_ANNO_TRAIN_FILE = 'data/train/train_anno.csv'
-    KEYPOINTS_TRAIN = 'data/train/keypoints_train.csv'
-    NUMBER_WORKERS = 8
-    BATCH_SIZE_TRAIN = 22
-    BATCH_SIZE_VAL = 2
-    NUMBER_EPOCHS = 500
-    TRANSFORMATION = True
-    SIZE_FULL_IMAGE = (256,512)
-    SIZE_TRUNK_IMAGE = (64,128)
-    SIZE_LIMB_IMAGE = (64,64)
-    STEPS_MAIN_OPT = [40, 80, 120, 160, 240, 320, 400] 
-    SCH_GAMMA = 0.5
-    SCH_WARMUP_FACTOR = 0.01
-    SCH_WARMUP_ITER = 25
-    NUM_CLASSES = 300
-    EMBEDDING_SIZE = 2560
-    ARCFACE = False
-    LR_MAIN = 0.00025
-    BACKBONE = 'resnet152'
-    DETERMINISTIC = [True, "warn"]
-    PRECISION = "16-mixed"
-    MIRRORED_DATA = True
-    INCLUDE_CAT_KEYPOINTS = True
-    MIN_IMAGES_PER_ENTITY = 8
-    BASE_MODEL = None
-    NAME_OUTPUT_MODEL = 'eval_model.pth'
-    RETRAIN = True
+def main(args):
 
-# Creating dataloader
-cat_data = ReidDataModule(data_directory=Config(),
-                            batch_size_train = Config().BATCH_SIZE_TRAIN,
-                            batch_size_val = Config().BATCH_SIZE_VAL,
-                            transform=Config().TRANSFORMATION,
-                            num_workers= Config().NUMBER_WORKERS, 
-                            size_full_image = Config().SIZE_FULL_IMAGE,
-                            size_trunk_image = Config().SIZE_TRUNK_IMAGE,
-                            size_limb_image = Config.SIZE_LIMB_IMAGE,
-                            mirrored_images = Config().MIRRORED_DATA,
-                            include_cat_keypoints=Config().INCLUDE_CAT_KEYPOINTS,
-                            min_images_per_entity = Config().MIN_IMAGES_PER_ENTITY)
-# Call the setup method
-cat_data.setup()
+    #Parameteres
+    class Config():
+        CAT_TRAINING_DIR = args.cat_training_dir
+        CAT_ANNO_TRAIN_FILE = args.cat_anno_train_file
+        KEYPOINTS_TRAIN = args.keypoints_train
+        NUMBER_WORKERS = args.number_workers
+        BATCH_SIZE_TRAIN = args.batch_size_train
+        BATCH_SIZE_VAL = args.batch_size_val
+        NUMBER_EPOCHS = args.number_epochs
+        TRANSFORMATION = args.transformation
+        SIZE_FULL_IMAGE = args.size_full_image
+        SIZE_TRUNK_IMAGE = args.size_trunk_image
+        SIZE_LIMB_IMAGE = args.size_limb_image
+        NUM_CLASSES = args.num_classes
+        EMBEDDING_SIZE = args.embedding_size
+        ARCFACE = args.arcface
+        BACKBONE = args.backbone
+        DETERMINISTIC = args.deterministic
+        PRECISION = args.precision
+        MIRRORED_DATA = args.mirrored_data
+        MIN_IMAGES_PER_ENTITY = args.min_images_per_entity
+        BASE_MODEL = args.base_model
+        NAME_OUTPUT_MODEL = args.name_output_model
+        RETRAIN = args.retrain
+        # Other parameters
+        INCLUDE_CAT_KEYPOINTS = True
+        LR_MAIN = 0.00025
+        STEPS_MAIN_OPT = [40, 80, 120, 160, 240, 320, 400] 
+        SCH_GAMMA = 0.5
+        SCH_WARMUP_FACTOR = 0.01
+        SCH_WARMUP_ITER = 25
 
-# Create our main loss function
-loss_fn = make_loss_with_parameters(Config().NUM_CLASSES)
+    # Creating dataloader
+    cat_data = ReidDataModule(data_directory=Config(),
+                                batch_size_train = Config().BATCH_SIZE_TRAIN,
+                                batch_size_val = Config().BATCH_SIZE_VAL,
+                                transform=Config().TRANSFORMATION,
+                                num_workers= Config().NUMBER_WORKERS, 
+                                size_full_image = Config().SIZE_FULL_IMAGE,
+                                size_trunk_image = Config().SIZE_TRUNK_IMAGE,
+                                size_limb_image = Config.SIZE_LIMB_IMAGE,
+                                mirrored_images = Config().MIRRORED_DATA,
+                                include_cat_keypoints=Config().INCLUDE_CAT_KEYPOINTS,
+                                min_images_per_entity = Config().MIN_IMAGES_PER_ENTITY)
+    # Call the setup method
+    cat_data.setup()
 
-# Create a model
-model = ReidCatModel(backbone_model= Config().BACKBONE,
-                number_classes=Config().NUM_CLASSES, 
-                embedding_size = Config().EMBEDDING_SIZE, 
-                main_loss = loss_fn,
-                steps_main_opt=Config().STEPS_MAIN_OPT,
-                lr_main = Config().LR_MAIN,
-                arcface = Config().ARCFACE,
-                num_epochs = Config().NUMBER_EPOCHS,
-                sch_gamma = Config().SCH_GAMMA,
-                sch_warmup_factor = Config().SCH_WARMUP_FACTOR,
-                sch_warmup_iter = Config().SCH_WARMUP_ITER)
+    # Create our main loss function
+    loss_fn = make_loss_with_parameters(Config().NUM_CLASSES)
 
-if Config().BASE_MODEL:
-    # Load the weights and biases
-    model.load_state_dict(torch.load('pretrained_weights/' + Config().BASE_MODEL))
-    print('loaded')
+    # Create a model
+    model = ReidCatModel(backbone_model= Config().BACKBONE,
+                    number_classes=Config().NUM_CLASSES, 
+                    embedding_size = Config().EMBEDDING_SIZE, 
+                    main_loss = loss_fn,
+                    steps_main_opt=Config().STEPS_MAIN_OPT,
+                    lr_main = Config().LR_MAIN,
+                    arcface = Config().ARCFACE,
+                    num_epochs = Config().NUMBER_EPOCHS,
+                    sch_gamma = Config().SCH_GAMMA,
+                    sch_warmup_factor = Config().SCH_WARMUP_FACTOR,
+                    sch_warmup_iter = Config().SCH_WARMUP_ITER)
+
+    if Config().BASE_MODEL:
+        # Load the weights and biases
+        model.load_state_dict(torch.load('pretrained_weights/' + Config().BASE_MODEL))
+        print('loaded')
 
 
-# Create a LearningRateMonitor callback
-lr_monitor = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
+    # Create a LearningRateMonitor callback
+    lr_monitor = pl.callbacks.LearningRateMonitor(logging_interval='epoch')
 
-# Define the Trainer
-trainer = Trainer(max_epochs=Config().NUMBER_EPOCHS,
-                accelerator='gpu', logger = True, 
-                enable_checkpointing=True, 
-                callbacks=[lr_monitor],
-                precision = Config().PRECISION, 
-                deterministic = Config().DETERMINISTIC)
+    # Define the Trainer
+    trainer = Trainer(max_epochs=Config().NUMBER_EPOCHS,
+                    accelerator='gpu', logger = True, 
+                    enable_checkpointing=True, 
+                    callbacks=[lr_monitor],
+                    precision = Config().PRECISION, 
+                    deterministic = Config().DETERMINISTIC)
 
-if Config.RETRAIN:
-    # Training
-    trainer.fit(model = model,
-            train_dataloaders=cat_data.train_dataloader(),
-            val_dataloaders=cat_data.val_dataloader())
+    if Config.RETRAIN:
+        # Training
+        trainer.fit(model = model,
+                train_dataloaders=cat_data.train_dataloader(),
+                val_dataloaders=cat_data.val_dataloader())
 
-# Save the weights and biases    
-torch.save(model.model.full_image_model.state_dict(),'pretrained_weights/' + Config.NAME_OUTPUT_MODEL)
+    # Save the weights and biases    
+    torch.save(model.model.full_image_model.state_dict(),'pretrained_weights/' + Config().NAME_OUTPUT_MODEL)
 
-# Printing message
-print('Training was done successfully! Model was saved in "pretrained weights directory"')
-    
+    # Printing message
+    print('Training was done successfully! Model was saved in "pretrained weights directory"')
+        
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--cat_training_dir", type=str, default='data/train/images/',
+                        help="Path of cat images for training")
+    parser.add_argument("--cat_anno_train_file", type=str, default='data/train/train_anno.csv',
+                        help="Filepath of annotations for training images")
+    parser.add_argument("--keypoints_train", type=str, default='data/train/keypoints_train.csv',
+                        help="Filepath of keypoints")
+    parser.add_argument("--number_workers", type=int, default = 8)
+    parser.add_argument("--batch_size_train",type = int, default=22)
+    parser.add_argument("--batch_size_val",type = int, default = 2)
+    parser.add_argument("--number_epochs", type = int, default = 500)
+    parser.add_argument("--transformation", type = bool, default= True,help = "Incorporate data augmentation")
+    parser.add_argument("--size_full_image", type = tuple, default = (256,512))
+    parser.add_argument("--size_trunk_image", type = tuple, default = (64,128))
+    parser.add_argument("--size_limb_image", type = tuple, default = (64,64))
+    parser.add_argument("--num_classes", type = int, default = 300)
+    parser.add_argument("--embedding_size", type = int, default = 2560)
+    parser.add_argument("--arcface", type = bool, default = False)
+    parser.add_argument("--backbone", type = str, default= "resnet152")
+    parser.add_argument("--deterministic", type = list, default = [True, "warn"])
+    parser.add_argument("--precision", type = str, default = "16-mixed")
+    parser.add_argument("--mirrored_data", type = bool, default = True, help = "Flip images to create new entities")
+    parser.add_argument("--min_images_per_entity", type = int, default = 8, help = "Ensuring a min of images per entity")
+    parser.add_argument("--base_model", default = None, help = "Use of a base model")
+    parser.add_argument("--name_output_model", default= 'eval_model.pth', help = "Name of trained weights")
+    parser.add_argument("--retrain", type = bool,default = True) 
+
+    args = parser.parse_args()
+
+main(args)       
 
